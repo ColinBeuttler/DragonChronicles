0402d4b0f49bf23ff0747f5c262fe68e08365fa3
Colin Beuttler <beutt102@mail.chapman.edu>
2023-03-06 22:58:34 -0800

    ghpagesfirst

---
+++ app/src/main/java/com/colinbeuttler/dragonchronicles/ChapterSelection.java
@@ -0,0 +1,14 @@
+package com.colinbeuttler.dragonchronicles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChapterSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapter_selection);
+    }
+}
