235843111ea00f899ffc418203e9e30a5b905625
colin <beutt102@mail.chapman.edu>
2023-03-08 21:56:28 -0800

    added class next dialogue

--- app/src/main/java/com/colinbeuttler/dragonchronicles/GameStart.java
+++ app/src/main/java/com/colinbeuttler/dragonchronicles/GameStart.java
@@ -3,12 +3,22 @@ package com.colinbeuttler.dragonchronicles;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GameStart extends AppCompatActivity {
+
+    TextView dialogue;
+
+    TextView nextDialogue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_start);
+
+        dialogue = findViewById(R.id.text_view_dialogue);
+        nextDialogue = findViewById(R.id.text_view_next);
+
+
     }
 }
