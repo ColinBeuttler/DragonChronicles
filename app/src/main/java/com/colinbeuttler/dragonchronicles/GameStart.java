ec2404835f289b810ecdc0de21f5c4306feb8ebe
colin <beutt102@mail.chapman.edu>
2023-03-08 22:06:10 -0800

    text change on click

--- app/src/main/java/com/colinbeuttler/dragonchronicles/GameStart.java
+++ app/src/main/java/com/colinbeuttler/dragonchronicles/GameStart.java
@@ -3,6 +3,7 @@ package com.colinbeuttler.dragonchronicles;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class GameStart extends AppCompatActivity {
@@ -20,5 +21,12 @@ public class GameStart extends AppCompatActivity {
         nextDialogue = findViewById(R.id.text_view_next);
 
 
+        nextDialogue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogue.setText("Hello there!!");
+            }
+        });
     }
+
 }
