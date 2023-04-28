9c20441300a1d98a0fb3da5fd4d17f762db2df26
colin <beutt102@mail.chapman.edu>
2023-04-28 14:44:22 -0700

    fixed cardview implementation errors

--- app/src/main/java/com/colinbeuttler/dragonchronicles/MainActivity.java
+++ app/src/main/java/com/colinbeuttler/dragonchronicles/MainActivity.java
@@ -1,12 +1,11 @@
 package com.colinbeuttler.dragonchronicles;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class MainActivity extends AppCompatActivity {
 
