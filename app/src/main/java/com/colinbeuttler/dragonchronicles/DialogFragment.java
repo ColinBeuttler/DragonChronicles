5ec89922eb02ddbbfed21d041cc81aa7689eee7f
Colin <beutt102@mail.chapman.edu>
2023-05-06 00:42:45 -0700

    trimming up

--- app/src/main/java/com/colinbeuttler/dragonchronicles/DialogFragment.java
+++ app/src/main/java/com/colinbeuttler/dragonchronicles/DialogFragment.java
@@ -28,16 +28,12 @@ public class DialogFragment extends androidx.fragment.app.DialogFragment {
         super.onViewCreated(view, savedInstanceState);
         dialogCard = (TextView) view.findViewById(R.id.text_view_dialogue);
         nextDialogue = (TextView) view.findViewById(R.id.text_view_next);
+        nextDialogue.setOnClickListener(view1 -> dialogCard.setText("Hello There!!"));
+
 //        continueDialogue();
-        dialogCard.setText("Hello there!!");
+//        dialogCard.setText("Hello there!!");
     }
 
-//    private void continueDialogue() {
-//        nextDialogue.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                dialogCard.setText("Hello there!!");
-//            }
-//        });
-//    }
+
+
 }
