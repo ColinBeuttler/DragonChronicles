04b8683df8b5cfb0ac7c0e40481944b28491a546
Colin <beutt102@mail.chapman.edu>
2023-05-08 16:51:52 -0700

    first dialog array run successful

--- app/src/main/java/com/colinbeuttler/dragonchronicles/DialogFragment.java
+++ app/src/main/java/com/colinbeuttler/dragonchronicles/DialogFragment.java
@@ -1,5 +1,6 @@
 package com.colinbeuttler.dragonchronicles;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +14,19 @@ public class DialogFragment extends androidx.fragment.app.DialogFragment {
 
     TextView dialogShadow;
 
+    int i = 0;
+
+    String[] dialogLines = {
+            "\n\tWelcome to Dragon Flight",
+            "\n\n\tChapter 1: An Egg and a Stranger.....\n\n",
+            "\n\t'Good Morning'",
+            "'Now where were we?'",
+            "\n\t'...Oh yes that's right!!!'",
+            "\n\t'You were about to pick a dragon egg...",
+            "\n\tDid you make a decision yet?'",
+
+    };
+
 
 
     @Nullable
@@ -25,10 +39,18 @@ public class DialogFragment extends androidx.fragment.app.DialogFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         dialogShadow = (TextView) view.findViewById(R.id.text_view_dialogue);
-        dialogShadow.setOnClickListener(view1 -> dialogShadow.setText("Hello There!!"));
+        dialogShadow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogShadow.setText(getLine());
+                i++;
+            }
+        });
 
     }
 
-
+public String getLine(){
+    return dialogLines[i];
+}
 
 }
