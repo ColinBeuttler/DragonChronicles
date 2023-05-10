973e63eb609ce46649611c6058975d6f23d98193
Colin <beutt102@mail.chapman.edu>
2023-05-10 12:25:15 -0700

    changed getLine to switch from if

--- app/src/main/java/com/colinbeuttler/dragonchronicles/DialogFragment.java
+++ app/src/main/java/com/colinbeuttler/dragonchronicles/DialogFragment.java
@@ -1,21 +1,20 @@
 package com.colinbeuttler.dragonchronicles;
 
-import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class DialogFragment extends androidx.fragment.app.DialogFragment {
 
     TextView dialogShadow;
+    LinearLayout dialogLayout;
 
     int i = 0;
 
@@ -27,7 +26,7 @@ public class DialogFragment extends androidx.fragment.app.DialogFragment {
             {"'Now where were we?'"},
             {"'...Oh yes that's right!!!'"},
             {"'You were about to pick a dragon egg..."},
-            {"Did you make a decision yet?'"}
+            {"Did you make a decision yet?'", "Yes", "No"}
     };
 
 
@@ -41,21 +40,49 @@ public class DialogFragment extends androidx.fragment.app.DialogFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         dialogShadow = (TextView) view.findViewById(R.id.text_view_dialogue);
+        dialogLayout = (LinearLayout) view.findViewById(R.id.dialog_linear_layout);
         dialogShadow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dialogShadow.setText(getLine());
-                i++;
+                if (dialogLines[i].length < 2) {
+                    i++;
+                }
             }
         });
 
     }
 
     public String getLine() {
-        if(dialogLines[i].length >2){
-            return dialogLines[i][0];
+        int dialogLength = dialogLines[i].length;
+        switch (dialogLength) {
+            case 1:
+                return dialogLines[i][0];
+            case 3:
+                addDialogOptions();
+                return dialogLines[i][0];
+            case 5:
+                addMultiAns();
+                return dialogLines[i][0];
+            default:
+                return null;
         }
-        else return null;
+    }
+
+    private void addMultiAns() {
+    }
+
+    private void addDialogOptions() {
+
+        for (int j = 1; j < dialogLines[i].length; j++) {
+            TextView option = new TextView(this.getContext());
+            option.setText(dialogLines[i][j]);
+            option.setId(j);
+            option.setLayoutParams(new ViewGroup.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT));
+            dialogLayout.addView(option);
+        }
     }
 
 }
