511433bb2286ab76fe580e608b322591235f5f96
Colin <beutt102@mail.chapman.edu>
2023-05-15 11:29:25 -0700

    Added login fragment

---
+++ app/src/main/java/com/colinbeuttler/dragonchronicles/LoginInFragment.java
@@ -0,0 +1,31 @@
+package com.colinbeuttler.dragonchronicles;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class LoginInFragment extends Fragment {
+
+    TextInputEditText userName;
+    TextInputEditText userPasword;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        userName.findViewById(R.id.user_name_text_input);
+        userPasword.findViewById(R.id.user_password_text_input);
+    }
+}
