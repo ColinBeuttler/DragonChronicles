e964b62c78af3502966c471e3cec11f2be834ac7
Colin <beutt102@mail.chapman.edu>
2023-05-17 13:31:33 -0700

    updated userInfo class

--- app/src/main/java/com/colinbeuttler/dragonchronicles/models/UserInfo.java
+++ app/src/main/java/com/colinbeuttler/dragonchronicles/models/UserInfo.java
@@ -2,34 +2,15 @@ package com.colinbeuttler.dragonchronicles.models;
 
 public class UserInfo {
 
-    private String userName;
-    private String password;
-
     private Dragon userDragon;
     private int currentChapter;
 
-    public UserInfo(String userName, String password, Dragon userDragon, int currentChapter) {
-        this.userName = userName;
-        this.password = password;
+    public UserInfo( Dragon userDragon, int currentChapter) {
+
         this.userDragon = userDragon;
         this.currentChapter = currentChapter;
     }
 
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
     public Dragon getUserDragon() {
         return userDragon;
