9d6670df049a54c2c378c90f9237ca58163cbf38
colin <beutt102@mail.chapman.edu>
2023-10-19 18:08:33 -0700

    successfully added junit for gendermessage()

---
+++ app/src/test/java/com/colinbeuttler/dragonchronicles/models/DragonTest.kt
@@ -0,0 +1,18 @@
+package com.colinbeuttler.dragonchronicles.models
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class DragonTest {
+
+    @org.junit.jupiter.api.Test
+    fun genderMessage() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getGender() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun determineGender() {
+    }
+}
